029c4b079858ed542892250d0c2bca3f2112d73c
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-02-24 03:17:52 +0530

    BrowserRouter update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/companies" element={<Companies />}></Route>
